e8bbc486e4ae8c67631dc9d32b4d70e9a73fe7d8
mazenwaelx <mazenwael575@gmail.com>
2023-07-13 15:45:45 -0700

    fourth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * return: Always 0 (Success)
+*/
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
