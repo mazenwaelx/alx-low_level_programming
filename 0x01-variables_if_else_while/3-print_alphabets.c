a49db15b4013bb3acdc8ac8901fab7abce380edb
mazenwaelx <mazenwael575@gmail.com>
2023-07-14 11:12:06 -0700

    sec

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Enter point
+ *
+ * Description: aa
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+        char ch = 'A';
+
+        while (ch <= 'Z')
+        {
+                putchar(ch);
+                ch++;
+        }
+        putchar('\n');
+return (0);
+}
