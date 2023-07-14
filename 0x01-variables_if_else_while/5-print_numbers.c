ea0780ae68b39dfbd8d4e692a56a0dcb15dc687d
mazenwaelx <mazenwael575@gmail.com>
2023-07-14 11:40:47 -0700

    five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Enter point
+ *
+ * Description: aa
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%i\n", num);
+	num++;
+}
+
+
+
+
+
+return (0);
+}
