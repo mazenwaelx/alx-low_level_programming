63825ba60e008fe0dca326942c348602ac597085
mazenwaelx <mazenwael575@gmail.com>
2023-07-18 07:37:27 -0700

    zero

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Enter point
+ *
+ * Description: aa
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+printf("_putchar\n");
+
+
+return (0);
+}
