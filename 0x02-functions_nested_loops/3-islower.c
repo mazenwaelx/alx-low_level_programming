4afcf244343badb754ba719bfacd8afac8141088
mazenwaelx <mazenwael575@gmail.com>
2023-07-18 18:05:30 -0700

    f

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- * _isalpha - chekcs if character is a letter
- * both lowercase
+ * _islower - chekcs if character is a letter
+ * is lowercase
  *
  * @c: takes input from other function
  *
- * Return: 1 is c if ture else 0;
+ * Return: 1 is 'c' is lowercase if ture else 0;
 */
 
-int _isalpha(int c)
+int _islower(int c)
 {
 if (c >= 97 && c <= 122)
-return (1);
-if (c >= 65 && c <= 90)
 return (1);
 return (0);
 }
