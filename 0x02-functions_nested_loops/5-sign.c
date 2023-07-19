02353b5a98ea992544194499bfd72fc45d1dd12d
mazenwaelx <mazenwael575@gmail.com>
2023-07-18 18:24:49 -0700

    five

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_sign - print + if n is greater than zero
+ * 0 if is zero and - if n is less than one
+ *
+ * @n: takes integer type input for function
+ *
+ * Return: 1 if +, 0 if 0 and -1 if -
+*/
+
+
+
+int print_sign(int n)
+{
+int i;
+
+if (i > 0)
+{
+_putchar('+');
+return (1);
+}
+if (i == 0)
+{
+_putchar('48');
+return (0);
+}
+if (i < 0)
+{
+_putchar('-');
+return (1);
+}
+return (0);
+}
