47e6c7bba7b45f901529f9c9a5207e3d9b43e4ca
mazenwaelx <mazenwael575@gmail.com>
2023-07-18 17:25:43 -0700

    first

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
  * print_alphabet - utilizes on the _putchar function to print
  * the alphabet a -z
  *
- * Return: Always 0.
- */
+*/
 void print_alphabet(void)
 {
 char ch = 'a';
-while (ch < 'z')
+while (ch <= 'z')
 {
 _putchar(ch);
 ch++;
