446601a1c637df6a5e7795e1dc36bb5f220493c8
mazenwaelx <mazenwael575@gmail.com>
2023-07-18 19:47:45 -0700

    yey

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Enter point
+ *
+ * Description: aa
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+int sum, num;
+for (num = 0; num < 1024; num++)
+{
+if ((num % 3 == 0) || (num % 5 == 0))
+sum += num;
+}
+printf("%d\n", sum);
+return (0);
+}
