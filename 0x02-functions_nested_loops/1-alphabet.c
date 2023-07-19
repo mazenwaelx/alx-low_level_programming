1d4068ba5985e920930e712570debafdfbbe9d7a
mazenwaelx <mazenwael575@gmail.com>
2023-07-18 17:23:44 -0700

    first

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,17 +3,20 @@
 /**
  * main - check the code
  *
+ * print_alphabet - utilizes on the _putchar function to print
+ * the alphabet a -z
+ *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-    char ch = 'a';
-    while (ch < 'z')
-    {
-	_putchar(ch);
-	ch++;
-    }
+char ch = 'a';
+while (ch < 'z')
+{
+_putchar(ch);
+ch++;
+}
 _putchar('\n');
 
-    return (0);
+
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * main - Enter point
+ * _putchar - writes the character c to sdout
+ * @c: the character to print
  *
- * Description: aa
- *
- * Return: 0 (Success)
+ * Return: On success 1.
+ *         On error, -1 is returned, and error is set appropriately.
 */
 
 int _putchar(char c)
 {
-return(write(1, &c, 1));
+return (write(1, &c, 1));
 }
