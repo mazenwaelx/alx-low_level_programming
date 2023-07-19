5771aa6a8764448ce40ff2628779262ece483310
mazenwaelx <mazenwael575@gmail.com>
2023-07-18 17:14:09 -0700

    first

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char ch = 'a';
+    while (ch < 'z')
+    {
+	_putchar(ch);
+	ch++;
+    }
+_putchar('\n');
+
+    return (0);
+}
