3e06c2268b37dc6ca04abe54baf247a62e15dbd7
mazenwaelx <mazenwael575@gmail.com>
2023-07-20 14:51:56 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - bbe
+*/
+
+void print_numbers(void)
+{
+int ch = 48;
+while (ch <= 57)
+{
+_putchar(ch)
+ch++;
+}
+_putchar('\n');
+}
