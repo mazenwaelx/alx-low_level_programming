a78d790badcc83851dc63632cf0c8a760eecf875
mazenwaelx <mazenwael575@gmail.com>
2023-07-20 15:39:41 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,18 @@
 
 void more_numbers(void)
 {
-int num, count;
+int num, count = 1, num2;
 
 while(count <= 10){
 for (num = 0; num <= 14; num++)
 {
-_putchar(num + 48);
+num2 = num;
+if (num > 9)
+{
+_putchar(1 +48)
+num2 = num % 10;
+}
+_putchar(num2 + 48);
 }
 count++;
 }
