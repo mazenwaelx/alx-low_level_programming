fd5897bb1ca3628c55bdab03e29ffcf62452e720
mazenwaelx <mazenwael575@gmail.com>
2023-07-20 14:15:46 -0700

    zero

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - aa
+ *@c: uo
+ * Return: 0 (Success)
+*/
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+return (0);
+}
