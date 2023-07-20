e89b4cb75933e4bd6c86747ec2d58df0f24081e6
mazenwaelx <mazenwael575@gmail.com>
2023-07-20 16:11:57 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - bbe
+ *@n: ff
+*/
+
+void print_diagonal(int n)
+{
+int num;
+
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (num = 1; num <= n; num++)
+{
+_putchar(' ') * n;
+_putchar('\')'
+_putchar('\n');
+}
+_putchar('\n');
+}
+}
