f7dfb6e994e1ee714df7b3b2372aeed088a3d4a0
mazenwaelx <mazenwael575@gmail.com>
2023-07-20 15:09:11 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - bbe
+ *
+*/
+
+void print_most_numbers(void)
+{
+int ch = 48;
+while (ch <= 57)
+{
+if (c == 50 || c == 52)
+continue;
+_putchar(ch);
+}
+_putchar('\n');
+}
