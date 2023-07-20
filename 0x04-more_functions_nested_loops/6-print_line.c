dc0742c9a638cdcfad244c2813146468c715a9a5
mazenwaelx <mazenwael575@gmail.com>
2023-07-20 15:58:47 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,14 @@
 void print_line(int n)
 {
 int num;
+if (n <= 0)
+_putchar('\n');
+else
+{
 for (num = 1; num <= n; num++)
 {
-if (n < 0)
-_putchar('\n');
 _putchar('_');
 _putchar('\n');
 }
-
+}
 }
