1c4ca3a58d803c67642e71bc76b8fa5c11f5a96c
mazenwaelx <mazenwael575@gmail.com>
2023-07-20 16:37:59 -0700

    eight

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - bbe
+ *@size: ff
+*/
+
+void print_square(int size)
+{
+int row;
+if (size <= 0)
+_putchar('\n')
+else
+{
+for (row = 1; row <= size; row++)
+{
+for (row = 1; row <= size; row++)
+{
+_putchar(35);
+}
+}
+_putchar('\n');
+}
+}
