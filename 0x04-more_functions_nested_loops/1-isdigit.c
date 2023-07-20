a709cd5b87007aebbc6cc93d2b7c00a08694e250
mazenwaelx <mazenwael575@gmail.com>
2023-07-20 14:22:04 -0700

    first

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - bbe
+ *
+ * @c: yoy
+ *Return: 0 (Success)
+*/
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+return (1);
+else
+return (0);
+return (0);
+}
