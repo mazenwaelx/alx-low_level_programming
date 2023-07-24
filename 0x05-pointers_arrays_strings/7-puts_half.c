248e065e8e50d931cf6035928c78e288c837f60b
mazenwaelx <mazenwael575@gmail.com>
2023-07-24 15:58:00 -0700

    go

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,10 @@ void puts_half(char *str)
 int len = 0, i;
 while (str[len] != '\0')
 len++;
-for (i = len / 2; i < len; len = i++)
+for (i = len / 2; i < len; i++)
 if(len % 2 != 0)
 {
-_putchar(str[i +1]);
+_putchar(str[i + 1]);
 }
 else
 _putchar(str[i]);
