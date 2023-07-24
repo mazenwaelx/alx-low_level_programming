66197320dcf3f5bf51ba63de11c1796373f72497
mazenwaelx <mazenwael575@gmail.com>
2023-07-24 15:37:30 -0700

    yogo

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - bbe
+ * @str: ff
+ * Return: Always 0
+*/
+
+void puts2(char *str)
+{
+int len;
+for (len = 0; len < str[len]; len = len + 2)
+_putchar(str[len]);
+_putchar('\n');
+}
