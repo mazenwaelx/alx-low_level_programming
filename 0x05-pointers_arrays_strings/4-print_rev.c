51c72932369df329b4f530adc0c70461b7864bd9
mazenwaelx <mazenwael575@gmail.com>
2023-07-24 14:24:22 -0700

    yoyo

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_rev - gg
+ * @s: gg
+ * Return: 0
+*/
+
+void print_rev(char *s)
+{
+int len, len2;
+while (*s != '\0')
+len++;
+for (len2 = len; len2 > 0; len2--)
+_putchar(*s);
+_putchar('\n');
+}
