cee98f738f49e84e5dc94f209002af52799ee83e
mazenwaelx <mazenwael575@gmail.com>
2023-07-24 16:13:08 -0700

    go

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_array - bbe
+ * @a: ff
+ * @n: bb
+ * Return: Always 0
+*/
+
+void puts_array(int *a, int n)
+{
+int len;
+for (len = 0; len < n; len++)
+{
+_putchar(a[len]);
+_putchar(',');
+_putchar(' ');
+}
+}
