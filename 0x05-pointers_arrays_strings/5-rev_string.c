5702090b7b0b433a0d5f2602280907fa70b086ba
mazenwaelx <mazenwael575@gmail.com>
2023-07-24 15:01:40 -0700

    yes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,7 +14,8 @@ while (s[len] != '\0')
 len++;
 for (len2 = 0; len2 < len; len2++)
 _putchar(s[len2]);
-for (len2 = len; len2 >= 0; len2--)
+_putchar('\n');
+for (len2 = len -1; len2 >= 0; len2--)
 {
 _putchar(s[len2]);
 }
