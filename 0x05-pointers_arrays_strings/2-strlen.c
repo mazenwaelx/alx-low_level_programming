b6d76b8dca9dd656a5dca81539f75c5fad2a4b6f
mazenwaelx <mazenwael575@gmail.com>
2023-07-24 14:00:27 -0700

    second

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * swap_int - bbe
+ * @s: gg
+ * Return: Always 0
+*/
+
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0' )
+{
+len++;
+str++;
+}
+return (len);
+}
