e0adb96c03f5d55363f1cc14b88b0114f70ad5b1
mazenwaelx <mazenwael575@gmail.com>
2023-07-24 16:45:48 -0700

    yo

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcoy - bbe
+ * @dest: ff
+ * @src: bb
+ * Return: Always 0
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; (src[i] != '\0') + 1; i++)
+src[i] = dest[i];
+}
