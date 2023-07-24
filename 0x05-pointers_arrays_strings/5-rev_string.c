84aadad849b6f7d45a81508e4add963166ff0033
mazenwaelx <mazenwael575@gmail.com>
2023-07-24 15:15:11 -0700

    yes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,11 @@
 void rev_string(char *s)
 {
 int len = 0, len2;
-
 while (s[len] != '\0')
-len++;
-len--;
-_putchar('\n');
-for (len2 = len - 1; len2 >= 0; len2--)
 {
-_putchar(s[len2]);
+len++;
 }
+for (len2 = len - 1; len2 >= 0; len2--)
+_putchar(s[len2]);
 _putchar('\n');
 }
