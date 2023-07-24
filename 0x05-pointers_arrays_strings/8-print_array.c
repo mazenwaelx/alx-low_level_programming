d345182cdc92eb06942397075aebb3385dad5046
mazenwaelx <mazenwael575@gmail.com>
2023-07-24 16:27:41 -0700

    go

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,11 @@
 void print_array(int *a, int n)
 {
 int len;
-for (len = 0; len <= n; len++)
+for (len = 0; len < n; len++)
 {
-printf("%d", a[len]);
+printf("%d", a[len]i);
 if (len != (n -1))
 printf(", ");
-else
-printf("\n");
 }
+printf("\n");
 }
