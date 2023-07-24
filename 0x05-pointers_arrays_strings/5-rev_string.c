ed4a06a3b2b33a5748a22ad8b3c544680ee332ec
mazenwaelx <mazenwael575@gmail.com>
2023-07-24 14:46:53 -0700

    yoyo

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * rev_string - gg
+ * @s: gg
+ * Return: 0
+*/
+
+void rev_string(char *s)
+{
+int len = 0, len2;
+while (s[len] != '\0')
+len++;
+for (len2 = len; len >= 0; len--)
+{
+_putchar(s[len]);
+}
+_putchar('\n')
+}
