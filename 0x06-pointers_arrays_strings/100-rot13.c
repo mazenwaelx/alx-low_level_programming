88eccbadb9d548e9c0136fe55ef23c2329ccb7a7
mazenwaelx <mazenwael575@gmail.com>
2023-07-26 18:07:35 -0700

    yo

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,13 +10,11 @@ char *rot13(char *str)
 char *p = str;
 while (*p)
 {
-if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M'))
+int offset = ((*p >= 'a') && (*p <= 'z')) ? 'a' : 'A';
+if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M') ||
+(*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z'))
 {
-*p += 13;
-}
-else if ((*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z'))
-{
-*p -= 13;
+*p = (*p - offset + 13) % 26 + offset;
 }
 p++;
 }
