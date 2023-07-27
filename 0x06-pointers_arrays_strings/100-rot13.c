b0e375867453bc1b6f96c0091f26397a909ab78e
mazenwaelx <mazenwael575@gmail.com>
2023-07-26 17:59:46 -0700

    bo

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - bbe
+ * @str: ff
+ * Return: Always 0
+*/
+char *rot13(char *str)
+{
+char *p = str;
+while (*p)
+{
+if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M'))
+{
+*p += 13;
+}
+else if ((*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z'))
+{
+*p -= 13;
+}
+p++;
+}
+return str;
+}
