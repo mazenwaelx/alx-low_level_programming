0dbe5eba979debd4caa1fec58feef358f9410b51
mazenwaelx <mazenwael575@gmail.com>
2023-07-31 09:29:52 -0700

    yo

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - bbe
+ * @s: ff
+ * @b: gg
+ * @n: ll
+ * Return: Always 0
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+for (unsigned int i = 0; i < n; i++)
+{
+*(s + i) = b;
+}
+return (s);
+}
