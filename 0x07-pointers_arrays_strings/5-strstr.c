7e181c67c20815856b354bdd3c2aa54bb63254df
mazenwaelx <mazenwael575@gmail.com>
2023-07-31 17:33:56 -0700

    ho

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strstr - bbe
+ * @haystack: ff
+ * @needle: gg
+ * Return: Always 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+
+char *a;
+while (*haystack)
+{
+for (a = needle; *a != '\0'; a++)
+{
+if (*haystack == *needle)
+return (haystack);
+}
+haystack++;
+}
+return (NULL);
+}
