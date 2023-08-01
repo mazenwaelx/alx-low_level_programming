fdb6516e2b466e8fcfa25727ab8eac2761250791
mazenwaelx <mazenwael575@gmail.com>
2023-07-31 18:11:48 -0700

    lol

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagsums - bbe
+ * @a: ff
+ * @size: bol
+ * Return: Always 0
+*/
+
+void print_diagsums(int *a, int size)
+{
+int i, sum = 0, sum2 = 0;
+for (i = 0; i < size; i++)
+{
+sum += a[i * size + i];
+}
+for (i = 0; i < size; i++)
+{
+sum2 += a[i * size + (size - 1 - i)];
+}
+printf("Sum of first diagonal: %d\n", sum);
+printf("Sum of second diagonal: %d\n", sum2);
+}
