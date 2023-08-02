d286be1c77fa504b6ee7e7fac6bfa72be83fa404
mazenwaelx <mazenwael575@gmail.com>
2023-08-02 16:01:29 -0700

    yo

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _sqrt_helper - bbe
+ * @n: ff
+ * @start: gg
+ * @end: gg
+ * Return: Always 0
+*/
+int _sqrt_helper(int n, int start, int end)
+{
+if (start > end)
+{
+return (-1);
+}
+int mid = (start + end) / 2;
+if (mid * mid == n)
+{
+return (mid);
+}
+else if (mid * mid > n)
+{
+return (_sqrt_helper(n, start, mid - 1));
+}
+else
+{
+return (_sqrt_helper(n, mid + 1, end));
+}
+}
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _sqrt_recursion - bbe
+ * @n: ff
+ * Return: Always 0
+*/
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+return (_sqrt_helper(n, 0, n));
+}
