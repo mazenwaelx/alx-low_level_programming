d752b001f5e3e75e596b1709a2ce886338ea1d91
mazenwaelx <mazenwael575@gmail.com>
2023-08-04 16:16:32 -0700

    yo

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Enter point
+ *
+ * Description: aa
+ * @argv: lol
+ * @argc: beo
+ * Return: 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+int i, sum;
+for (i = 0; i < argc; i++)
+{
+if (argv(argc) < '0' || argv(argc) > '9')
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+sum+= atoi(argv[i]);
+}
+}
+printf("%d\n", sum);
+return (0);
+}
