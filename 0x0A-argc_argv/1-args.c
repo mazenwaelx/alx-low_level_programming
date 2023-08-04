70599c0f4f1d29313aa5bd1091d3bfdb23829957
mazenwaelx <mazenwael575@gmail.com>
2023-08-04 15:22:03 -0700

    lol

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ *
+ * Description: aa
+ * @argv: yo
+ * @argc: bel
+ * Return: 0 (Success)
+*/
+
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%i\n", argc);
+return (0);
+}
+
