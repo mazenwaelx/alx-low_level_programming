3582f04580ee1cea0093e2b88f283d66933a5c67
mazenwaelx <mazenwael575@gmail.com>
2023-08-04 18:17:44 -0700

    yo

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - Enter point
+ * main - Entry point
  *
- * Description: aa
- * @argv: lol
- * @argc: beo
+ * Description: Calculates the minimum number of coins required to make change for an amount of money
+ * @argv: Command-line arguments
+ * @argc: Number of command-line arguments
  * Return: 0 (Success)
 */
 
@@ -18,6 +18,7 @@ printf("Error\n");
 return (1);
 }
 int cents;
+
 cents = atoi(argv[1]);
 if (cents < 0)
 {
@@ -25,15 +26,19 @@ printf("0\n");
 return (0);
 }
 int quarters;
+
 quarters = cents / 25;
 cents -= quarters * 25;
 int dimes;
+
 dimes = cents / 10;
 cents -= dimes * 10;
 int nickels;
+
 nickels = cents / 5;
 cents -= nickels * 5;
 int pennies;
+
 pennies = cents;
 printf("%d\n", quarters + dimes + nickels + pennies);
 return (0);
