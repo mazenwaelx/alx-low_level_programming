c9b3ac575204d72a7bf0dbbd540a5dad1d91ed18
mazenwaelx <mazenwael575@gmail.com>
2023-08-04 17:52:50 -0700

    yo

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _puts - gg
+ * @str: gg
+ * Return: 0
+*/
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
