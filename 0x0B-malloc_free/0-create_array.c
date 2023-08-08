341f79656877e952f39faacd2fbe83fb5ac4018e
mazenwaelx <mazenwael575@gmail.com>
2023-08-08 09:29:05 -0700

    lol

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,10 @@ unsigned int i;
 if (size <= 0)
 return (NULL);
 ar = (char *)malloc(size * sizeof(char));
+if (ar == NULL)
+{
+return (NULL);
+}
 for (i = 0; i < size; i++)
 ar[i] = c;
 return (ar);
