68d627fc7d781978d631fcb79d09d983c73cfc0a
mazenwaelx <mazenwael575@gmail.com>
2023-08-08 13:46:45 -0700

    bo

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,12 +14,12 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-while(s1[len1] != '\0')
+while (s1[len1] != '\0')
 len1++;
-while(s2[len2] != '\0')
+while (s2[len2] != '\0')
 len2++;
-ar = (char *)malloc((len1 + len2) * sizeof(char));
-for(i = 0; i < len1; i++)
+ar = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+for (i = 0; i < len1; i++)
 ar[i] = s1[i];
 for (i = 0; i < len2; i++)
 ar[len1 + i] = s2[i];
