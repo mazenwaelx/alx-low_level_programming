025d3121f11df0b25da42c45f3a5471e306b3da6
mazenwaelx <mazenwael575@gmail.com>
2023-08-14 17:41:38 -0700

    go

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,5 +12,5 @@ if (d == NULL)
 return;
 printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
 printf("Age %.1f\n", d->age);
-printf("Owner: %s\n", d->owner != NULL ? d->name : "(nil)");
+printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
