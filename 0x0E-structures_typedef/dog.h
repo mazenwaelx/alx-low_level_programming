d9fdd3c2884cfa54719fd82886036bc304f7c948
mazenwaelx <mazenwael575@gmail.com>
2023-08-14 18:28:01 -0700

    bo

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,8 +17,7 @@ float age;
 char *owner;
 };
 typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner)
-void free_dog(dog_t *d)
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_H */
