9dc382fe2a0d6119bc97148210dc2b8d5ffba838
mazenwaelx <mazenwael575@gmail.com>
2023-08-16 16:27:00 -0700

    lol

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,8 +8,8 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int i;
+int *end = array + size - 1;
 if (array && size && action)
-for (i = 0; i < size; i++)
-action(array[i]);
+while (array <= end)
+action(*array++);
 }
