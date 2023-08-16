b8d0e77e81c5b19c7c71323280f881b3df90b576
mazenwaelx <mazenwael575@gmail.com>
2023-08-16 16:17:32 -0700

    lol

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - bbe
+ * @array: ff
+ * @size: gg
+ * @action: gg
+ * Return: Always 0
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+for (i = 0; i <= size; i++)
+action(array[i]);
+}
