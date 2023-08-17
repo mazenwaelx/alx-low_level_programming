645659a8c569bdc1ae0b457d74db728af6975f74
mazenwaelx <mazenwael575@gmail.com>
2023-08-16 17:39:22 -0700

    lol

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,10 +18,10 @@ op_t ops[] = {
 {"%", op_mod},
 {NULL, NULL}
 };
-int i;
-while (ops[i].op)
+int i = 0;
+while (i < 5)
 {
-if (*(ops[i].op) == *s)
+if (s && s[0] == ops[i].op[0] && !s[1])
 return (ops[i].f);
 i++;
 }
