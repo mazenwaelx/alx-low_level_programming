d78abca21d37fc75b7c33258626235125049219b
mazenwaelx <mazenwael575@gmail.com>
2023-08-17 15:02:06 -0700

    lol

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - bbe
+ * @separator: ff
+ * @n: gg
+ * @...: gg
+ * Return: Always 0
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+va_start(args, n);
+int i, value;
+for (i = 0; i < n; i++)
+{
+value = va_arg(args, int);
+printf("%d", value);
+if (separator != NULL && i < n - 1)
+printf("%s", separator);
+}
+va_end(args);
+printf("\n");
+}
+int main(void)
+{
+print_numbers(", ", 4, 0, 98, -1024, 402);
+return (0);
+}
