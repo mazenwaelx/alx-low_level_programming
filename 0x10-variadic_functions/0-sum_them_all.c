2a60fbb6fef3d61832fafcc21ac92d1b95467f8a
mazenwaelx <mazenwael575@gmail.com>
2023-08-17 14:43:35 -0700

    go

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,12 @@ if (n == 0)
 return (0);
 va_list args;
 int i, sum;
-va_start (args, n);
+va_start(args, n);
 sum = 0;
 for (i = 0; i < n; i++)
 {
-sum += va_arg (args, unsigned int);
+sum += va_arg(args, unsigned int);
 }
-va_end (args);
+va_end(args);
 return (sum);
 }
