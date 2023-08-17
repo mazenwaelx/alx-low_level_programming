1a41488f993115ef10a90485a19672fc1ddda385
mazenwaelx <mazenwael575@gmail.com>
2023-08-17 15:20:50 -0700

    bo

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,10 +8,10 @@
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list args;
-va_start(args, n);
 unsigned int i;
 const char *str;
+va_list args;
+va_start(args, n);
 for (i = 0; i < n; ++i)
 {
 str = va_arg(args, const char *);
