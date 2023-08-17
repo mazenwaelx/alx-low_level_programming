4ed5763279259780c4a5701754766128087a88ce
mazenwaelx <mazenwael575@gmail.com>
2023-08-17 16:36:05 -0700

    go

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,7 +9,7 @@
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-int i =n;
+int i = n;
 char *str;
 va_list ap;
 if (!n)
@@ -18,7 +18,7 @@ printf("\n");
 return;
 }
 va_start(ap, n);
-while(i--)
+while (i--)
 printf("%s%s", (str = va_arg(ap, char*)) ? str : "(nil)",
 i ? (separator ? separator : "") : "\n");
 va_end(ap);
