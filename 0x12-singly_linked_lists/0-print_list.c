8b10bac79b8f703203612b368dfc2cdd3590c7a4
mazenwaelx <mazenwael575@gmail.com>
2023-08-24 16:25:17 -0700

    lol

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,20 @@
 #include "lists.h"
 /**
+ * _strlen - return the length
+ * @s: lol
+ * Return: lol
+*/
+
+int _strlen(char *s)
+{
+int i = 0;
+if (!s)
+return (0);
+while (*s++)
+i++;
+return (i);
+}
+/**
  * print_list - lol
  * @h: lol
  *
@@ -10,14 +25,11 @@
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
-while (h != NULL)
+while (h)
 {
-if (h != NULL)
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", h->str);
-count++;
+printf"([%d] %s\n", _strlen (h->str), h->str ? h->str : "(nil)");
 h = h->next;
+count++;
 }
 return (count);
 }
