25f907cfe05a53a6864ea614bf2b7711b371d410
mazenwaelx <mazenwael575@gmail.com>
2023-08-24 16:33:56 -0700

    lol

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - lol
+ * @h: lol
+ * 
+ * Return: always 0
+*/
+
+
+size_t list_len(const list_t *h)
+{
+size_t i = 0;
+while (h != NULL)
+{
+i++;
+h = h->next;
+}
+return (i);
+}
