7e5b4d812f466fa767a6d50defc59ee234295427
mazenwaelx <mazenwael575@gmail.com>
2023-08-24 16:49:05 -0700

    lol

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,21 +17,20 @@ return (i);
 }
 
 /**
- * list_len - lol
- * @h: lol
- *
+ * add_node - lol
+ * @head: lol
+ * @str: lol
  * Return: always 0
 */
 
 
-list_t *add_node(list_t **head, const char *str) {
+list_t *add_node(list_t **head, const char *str)
+{
 if (str == NULL)
 return (NULL);
-    
 list_t *new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
-    
 new_node->str = strdup(str);
 if (new_node->str == NULL)
 {
@@ -41,6 +40,5 @@ return (NULL);
 new_node->len = _strlen(str);
 new_node->next = *head;
 *head = new_node;
-    
 return (new_node);
 }
