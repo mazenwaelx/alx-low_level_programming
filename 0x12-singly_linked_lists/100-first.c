2ab3f1cb3aaac7974e5d0de96c810672534cc80c
mazenwaelx <mazenwael575@gmail.com>
2023-08-24 17:34:36 -0700

    lol

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void pre_main(void) __attribute__((constructor));
 
 void pre_main(void)
 {
-printf("You're beat! and yet, you must allow,\n")"
+printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
 }
