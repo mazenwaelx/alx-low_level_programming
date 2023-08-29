f36f942978d24c8d52b49e396adf4093866d2a5b
mazenwaelx <mazenwael575@gmail.com>
2023-08-29 04:24:57 -0700

    lol

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
+ * listint_len - Returns the number of elements in a linked listint_t list.
  * @h: Pointer to the head of the list.
  *
- * Return: The number of nodes.
+ * Return: The number of elements in the list.
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 size_t count = 0;
 while (h != NULL)
