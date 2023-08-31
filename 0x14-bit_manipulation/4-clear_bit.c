1c5e97434839703902b0a46bcfea986908902c3d
mazenwaelx <mazenwael575@gmail.com>
2023-08-31 15:08:37 -0700

    lol

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * clear_bit - Prints the binary representation of a number.
+ * @n: Unsigned long int number to print in binary.
+ * @index: lol
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index > (sizeof(unsigned long int) * 8 - 1))
+return (-1);
+*n &= ~(1UL << index);
+return (1);
+}
